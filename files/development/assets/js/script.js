c046e15c5bd1069bae0413391c0a20b8cb5bf40e
aoki <sena.rn68@gmail.com>
2014-08-18 22:12:28 +0900

    update

--- files/development/assets/js/script.js
+++ files/development/assets/js/script.js
@@ -1,5 +1,24 @@
 $(function(){
-  $(".item-list .item .detail").on("click",function(){
+
+  var book = $('#archive-stand .shelf .item-list .item .detail'),
+      gn_select = $('#head #g-nav .select');
+
+  $(book).on("click",function(){
     $(this).toggleClass('look');
+  });
+
+  $([gn_select[0],book[0]]).on("click",function(){
+    $("#wrapper").toggleClass('archives');
+  });
+
+
+  // pjax
+  $(document).on('click', '.pjax', function(e) {
+    e.preventDefault();
+    $.pjax({
+      container : '#pjax-test',
+      fragment : '#contents',
+      timeout : 1000
+    });
   });
 });
